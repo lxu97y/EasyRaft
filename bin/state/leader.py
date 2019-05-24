2cf16f37695fdcd6bf811fa5bd0d99a34f33b6c3
freezeX <39030019+freezeX@users.noreply.github.com>
2019-05-23 22:22:19 -0500

    fix election bug

--- bin/state/leader.py
+++ bin/state/leader.py
@@ -12,6 +12,10 @@ class Leader(State):
         self.matchIndex = dict()
         self.heartbeat
 
+    def handle_vote_request(self,message):
+    #method override, leader should refuse any vote request unless the one from newer term(in this case it should convert to follower)
+        self.send_vote_response(message, False)
+
     def receive_append_entries_response(self,message):
         pass
         # to do
